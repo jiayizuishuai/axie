2eed8ae860fe04b16cced6b094b3a6c1cc937206
jiayizuishuai <972111778@qq.com>
2022-06-30 19:23:52 +0800

    1

---
+++ axie_origin_integrated/test.py
@@ -0,0 +1,3 @@
+import os
+i = os.path.abspath("axie_origin_integrated/env_src/envs_axie/config/card2id.yaml")
+i=0
--- core/agent_interact.py
+++ core/agent_interact.py
@@ -246,30 +246,32 @@ class DMCV3AgentInteract(BaseAgentInteract):
     def get_hidden_init(self):
         pass
 
-    def get_action(self, position, sim, flags=None):
+    def get_action(self, position, obs, flags=None):
         # obs = {
         #   'state': the state of two players and battle infos
         #   'raw_legal_actions' : the list of actions (card_name, card_target)
         #   'legal_actions' : the list of encoded actions (card_name, card_target)
         # }
-
-        # self.obs_x_no_action_buf[position].append(th.Tensor(obs['state']))
+        self.obs_x_no_action_buf[position].append(th.Tensor(obs['state']))
 
         response = self._send(
             {
                 "command": GET_ACTION,
                 "position": position,
-                "sim": sim,
+                "obs": obs,
                 "flags": flags
             }
         )
 
-        response = response['response']
+        # _action_idx = int(agent_output['action'].cpu().detach().numpy())
+        _action_idx = response['action']
+        action = obs['legal_actions'][_action_idx]
+        encoded_action = obs['encoded_legal_actions'][_action_idx]
 
-        self.obs_action_buf[position].append(th.Tensor(response['encoded_action']))
+        self.obs_action_buf[position].append(th.Tensor(encoded_action))
         self.size[position] += 1
 
-        return response['action']
+        return action
 
     def log_reward_info(self, reward, infos):
         pass
